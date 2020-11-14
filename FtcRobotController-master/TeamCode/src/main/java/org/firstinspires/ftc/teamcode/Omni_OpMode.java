d1a6e533097e495e7d551442b7ddc1a3ae809cf1
sparklefbanks <31677932+sparklefbanks@users.noreply.github.com>
2020-11-14 15:25:23 -0800

    Fixed motor rotations

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omni_OpMode.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omni_OpMode.java
@@ -75,7 +75,7 @@ public class Omni_OpMode extends LinearOpMode {
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
         centerDrive.setDirection(DcMotor.Direction.FORWARD);
 
