c328d23e9abd9a33d6d3d3d128c29cb316c5130b
sparklefbanks <31677932+sparklefbanks@users.noreply.github.com>
2021-02-13 13:37:38 -0800

    Basic Branch Setup && Wobble Goal Class basic framework

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleGoal.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class WobbleGoal {
+    // Set up Hardware Devices
+        /* Motors */
+    DcMotor linSlide;
+    double slidePower = 1.0;
+        /*Servos*/
+    CRServo claw;
+    /*** CLAW Positions ***/
+    int neutral = 0;
+    int grab = ;
+    int store = ;
+
+    //Constructors
+    public WobbleGoal(DcMotor linearSlide, CRServo servo){
+        linSlide = linearSlide;
+        intake = claw;
+    }
+    //Methods
+    /***SERVO METHODS***/
+    public void setClaw(int pos) {
+        this.claw.setPosition(pos);
+    }
+
+    public void storeClaw(){
+        this.setClaw(store);
+    }
+    public void activateClaw() {
+        this.setClaw(grab);
+    }
+    public void neturalClaw(){
+        this.setClaw(neutral);
+    }
+    public void zeroPosition(){
+        this.setClaw(0);
+    }
+    /*** LINEAR SLIDE METHODS ***/
+    public void raiseWobbleGoal(){
+        linSlide.setPower(slidePower);
+    }
+    public void lowerWobbleGoal(){
+        linSlide.setPower(-1*slidePower);
+    }
+
+    public void deliverWobbleGoal(){
+        //lower linear slide  timed??
+        //move claw to its stored position
+    }
+}
